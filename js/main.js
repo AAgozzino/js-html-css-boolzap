d7b8910e48321f1da6fecd55f46b078ff4b3803a
Anna Agozzino <anna.agozzino91@gmail.com>
2020-09-08 21:42:41 +0200

    Template messaggio inviato + JavaScript invio messaggio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  
+  $("#user-message").keyup(
+    function(sendMessage){
+      if (sendMessage.which == 13) {
+        var textNewMessage = $("#user-message").val();
+        console.log(textNewMessage);
+        var newMessage = $(".template .chat-baloon").clone();
+        $(".chat").append(newMessage);
+        $(newMessage).children(".text-message").append(textNewMessage);
+        console.log(newMessage);
+        $("#user-message").val("");
+      }
+    }
+  );
+
 });
